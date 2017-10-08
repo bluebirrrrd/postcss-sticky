10c90a89fb41c6e14a66f5422560d826eeb99e4d
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-10-08 08:31:08 +0300

    move offset declarations to @supports

--- index.js
+++ index.js
@@ -2,11 +2,33 @@
 const postcss = require("postcss")
 const {name} = require("./package")
 
+const offsets = [
+  "top",
+  "right",
+  "bottom",
+  "left",
+]
+
+const isOffset = node => {
+  return node.type == "decl" && offsets.includes(node.prop)
+}
+
 const plugin = root => root.walkDecls(decl => {
   if (decl != "position: sticky") return
 
   decl.cloneBefore({
     value: "relative",
+  })
+
+  let rule = decl.parent
+
+  rule.before({
+    name: "supports",
+    params: `(${decl})`,
+    nodes: {
+      selector: rule.selector,
+      nodes: rule.nodes.filter(isOffset),
+    },
   })
 })
 
